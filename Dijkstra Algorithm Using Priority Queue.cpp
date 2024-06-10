f470d5461a55dde605775efc9d7af08c07be81db
Anshu Mehta <125853845+anshux2211@users.noreply.github.com>
2024-06-10 10:35:22 +0530

    Updated the mistake learning from strivers video

--- Dijkstra Algorithm Using Priority Queue.cpp
+++ Dijkstra Algorithm Using Priority Queue.cpp
@@ -1,24 +1,27 @@
 // S is Source vertex
 // adj[i] is a list of lists containing two integers where the first integer of each list j denotes there is edge between i and j , second integers corresponds to the weight of that  edge.
     
-    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S) 
     {
-        vector<int> ans(V,INT_MAX); // To Store shortest Path of each vertex from source
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;  // Min Priority Queue that stores pair in which second element will be vertex V and First element will be distance from to vertex V.
-        pq.push({0,S}); // Initially we push Source vertex and its distance as 0.
+        // Code here
+        vector<int> dist(V,INT_MAX); // To store shortest path initially all are set infinity
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;    // Min Priority Queue Containing pair element. 2nd Element is node and 1st element is distance from source to that node.
+        dist[S]=0;    // Inititally source is set 0.
+        pq.push({0,S});    // And Source node is pushed into the queue.
         
-        while(pq.size()!=0)  
+        while(pq.size()!=0)    // Util queue is not empty
         {
-            pair<int,int> curr=pq.top();  // This will get top element of Priority Queue that will be minimum of all.
+            pair<int,int> curr=pq.top();    // Top Element
             pq.pop();
-            if(ans[curr.second]==INT_MAX) // If it is not visited till now, It will be minimum for sure
+            for(auto&i:adj[curr.second])    // Seraching all the paths we can go from this current node
             {
-                ans[curr.second]=curr.first;  // Update it
-                for(auto&i:adj[curr.second])  // Traverse to all its adjacent vertex and push it in Priority Queue
+                int tmp=curr.first+i[1];    // And how much time will we take
+                if(dist[i[0]]>tmp)            // If we take less distance then we replace it with previous and push it to the queue
                 {
-                        pq.push({curr.first+i[1],i[0]});  
+                    dist[i[0]]=tmp;
+                    pq.push({tmp,i[0]});
                 }
             }
         }
-        return ans;
+        return dist;
     }
